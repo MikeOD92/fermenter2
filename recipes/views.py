56e73082d5670097988312e16e65be2e4b8670e4
Michael O'Dell <michaelodell660@gmail.com>
2024-07-24 12:01:43 -0700

    fixing index view, renaming models import

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-from .models import *
+from recipes.models import Recipe
 from .serializers import * 
 
 def index(request): 
-    queryset = models.Recipe.objects.all()
+    queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     return HttpResponse("hello world")
 # Create your views here.
